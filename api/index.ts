fc7364f8a22ae436945d3540121ddab758e6cb3f
julyanpatricio <julyan.patricio@gmail.com>
2021-08-27 01:24:27 -0300

    connect database

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,14 @@
 import app from './src/app';
+import {sequelize} from './src/db';
 
-app.listen(3001, function () {
-	console.log('App is listening on port 3001!');
-});
+
+sequelize
+	.sync({force: true, logging: false})
+	.then(() => {
+		console.log('base de datos conectada! :D');
+		app.listen(3001, function () {
+			console.log('App is listening on port 3001!');
+		});
+	})
+	.catch((err) => console.error(err));
+  
--- api/src/lib/config.ts
+++ api/src/lib/config.ts
@@ -5,13 +5,13 @@ dotenv.config();
 const config = {
 	dbUser: process.env.DB_USER,
 	dbPassword: process.env.DB_PASSWORD,
-	dbHost: process.env.DB_HOST || 'localhost',
-	dbName: process.env.DB_NAME || 'workshop',
-	dbPort: process.env.DB_PORT || '5432',
+	dbHost: process.env.DB_HOST,
+	dbName: process.env.DB_NAME,
+	dbPort: process.env.DB_PORT,
 	dev: process.env.NODE_ENV !== 'production',
-	port: process.env.API_PORT || '3001',
-	host: process.env.API_host || 'localhost',
-	cors: process.env.CORS || 'localhost:3000',
+	port: process.env.API_PORT,
+	host: process.env.API_host,
+	cors: process.env.CORS,
 };
 
 export default config;
