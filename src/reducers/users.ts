73fd254b272ce811a3a85e61761dad6ba1c6dbab
julyanpatricio <julyan.patricio@gmail.com>
2021-08-31 19:48:29 -0300

    extend reducer

--- src/reducers/users.ts
+++ src/reducers/users.ts
@@ -1,10 +1,15 @@
-import {User, FetchUsersAction} from '../actions';
-import {ActionTypes} from '../actions/types';
+import {User} from '../actions';
+import {Action, ActionTypes} from '../actions/types';
 
-export const usersReducer = (state: User[] = [], action: FetchUsersAction) => {
+export const usersReducer = (state: User[] = [], action: Action) => {
+
 	switch (action.type) {
 		case ActionTypes.fetchUsers:
 			return action.payload;
+
+		case ActionTypes.deleteUser:
+			return action.payload;
+
 		default:
 			return state;
 	}
